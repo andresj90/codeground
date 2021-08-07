c3b0a9f17608636702d445f5554a945b0dd10ab4
andresj90 <andresf.jaran@konradlorenz.edu.co>
2021-08-06 22:21:22 -0500

    project setup/ added new configuration files, typescript for webpack and new libraries, do npm install

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,14 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require("path");
 module.exports = {
-    entry: { index: path.resolve(__dirname, "src", "index.js") },
+    entry: { index: path.resolve(__dirname, "src", "index.tsx") },
+    devtool: 'inline-source-map',
     output: {
-        path: path.resolve(__dirname, "build")
+        path: path.resolve(__dirname, "build"),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
-        })
+        }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+            dry: true,
+        }),
     ],
     module: {
         rules: [
@@ -20,7 +27,15 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
-              }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ]
-    }
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', 'jsx'],
+    },
 }
